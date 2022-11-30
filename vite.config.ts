c5ec0ac5133638686d7fc655ce5c65b7d78f65d9
Hyunwoo Kim <admin@hyunwoo.kim>
2022-11-30 21:24:58 +0900

    Add Path Alias & Default Styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/styles/common.scss';`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [vue()],
+});
