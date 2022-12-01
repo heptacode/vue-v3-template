d8a30798c2321f04aea8be2682345cb1359769ea
Hyunwoo Kim <admin@hyunwoo.kim>
2022-12-01 19:28:42 +0900

    Add @iconify/vue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import App from '@/App.vue';
 import { router } from '@/router.js';
 import '@/styles/index.scss';
+import { Icon } from '@iconify/vue';
 import { createPinia } from 'pinia';
 import { createPersistedState } from 'pinia-plugin-persistedstate';
 import { createApp } from 'vue';
 
 const app = createApp(App);
+
+app.component('Icon', Icon);
 
 app.use(router);
 
