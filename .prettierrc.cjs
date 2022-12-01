f24fd0aded7a974bedc6e3bb85f89dd76041a2fa
Hyunwoo Kim <admin@hyunwoo.kim>
2022-12-01 21:25:25 +0900

    Make Prettier Config CJS

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  arrowParens: 'avoid',
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
